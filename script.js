4acf1510c87ea89869686a3957887708f563b539
asje2005 <101301416+asje2005@users.noreply.github.com>
2023-12-18 16:59:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+//Define HTML Elements//
+
+const board = document.getElementById('game-board');
+
+//Draw game map, snake, food
+function draw() {
+    board.innerHTML = '';
+    drawSnake();
+}
+
+//Draw Snake
+function darwSnake() {
+    
+}
